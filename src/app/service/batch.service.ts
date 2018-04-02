873a27567041b46af98ee88d874b6a77190d006c
kr620345 <kr620345@amway.com>
2018-04-02 20:21:16 +0900

    근무종료 시 batch end 하면서 토큰 초기화
    근무종료 시 batch end 하면서 토큰 초기화

--- src/app/service/batch.service.ts
+++ src/app/service/batch.service.ts
@@ -1,13 +1,17 @@
-import { Config } from './../core/config/config';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { InfoBroker } from './../broker/info.broker';
+import { Config } from './../core/config/config';
 import { Logger } from './pos';
 
 @Injectable()
 export class BatchService {
 
-  constructor(private http: HttpClient, private config: Config, private logger: Logger) { }
+  constructor(
+              private http: HttpClient,
+              private infoBroker: InfoBroker,
+              private config: Config, private logger: Logger) { }
 
   /**
    * Start Shift
@@ -24,8 +28,9 @@ export class BatchService {
    * 로그오프 시 배치 저장 후(POS 종료 확인 팝업 -> 배치 정보 저장  팝업 뜸) 대시보드 메인으로 이동
    */
   endBatch() {
-    this.logger.debug('*** end batch, and session storage clear...', 'batch.service');
-    sessionStorage.clear();
+    this.logger.debug('*** end batch, and session storage access token info remove...', 'batch.service');
+    sessionStorage.removeItem('tokenInfo'); // remove access token info
+    this.infoBroker.sendInfo(null); // info broker에 null access token을 전송해서 초기 상태로 변경.
   }
 
 
