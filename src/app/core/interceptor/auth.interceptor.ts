4a3dea77ef8d2f9861e23036fcdac015bf742f61
kr620345 <kr620345@amway.com>
2018-04-04 21:02:49 +0900

    수정
    수정

--- src/app/core/interceptor/auth.interceptor.ts
+++ src/app/core/interceptor/auth.interceptor.ts
@@ -1,18 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
+import { LoginService } from '../../service/pos';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+
+  constructor(private loginService: LoginService) { }
+
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const currentUserInfo = JSON.parse(sessionStorage.getItem('tokenInfo'));
+    const currentUserInfo = this.loginService.getTokenInfo(); // JSON.parse(sessionStorage.getItem('tokenInfo'));
 
     if (currentUserInfo) {
-        request = request.clone({
-            setHeaders : {
-                Authorization : `${currentUserInfo.token_type} ${currentUserInfo.access_token}`
-            }
-        });
+      request = request.clone({
+          setHeaders : {
+              Authorization : `${currentUserInfo.token_type} ${currentUserInfo.access_token}`
+          }
+      });
     }
 
     return next.handle(request);
