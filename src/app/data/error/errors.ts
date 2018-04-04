f6d6a6b0e0826252b0d927c64418819218fddcf2
kr620345 <kr620345@amway.com>
2018-04-04 16:50:01 +0900

    Erros 객체 유형 추가
    Erros 객체 유형 추가

--- src/app/data/error/errors.ts
+++ src/app/data/error/errors.ts
@@ -1,8 +1,14 @@
 export class Errors {
     type: string;
     message: string;
-    constructor(type: string, message: string) {
+    reason: string;
+    subject: string;
+    subjectType: string;
+    constructor(type: string, message: string, reason: string, subject: string, subjectType: string) {
         this.type = type;
         this.message = message;
+        this.reason = reason;
+        this.subject = subject;
+        this.subjectType = subjectType;
     }
 }
