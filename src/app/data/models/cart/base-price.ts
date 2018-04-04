728106c99fd50d571e2fe65eee718056debf160d
kr620345 <kr620345@amway.com>
2018-04-04 20:45:47 +0900

    Add to Cart 객체 수정
    Add to Cart 객체 수정

--- src/app/data/models/cart/base-price.ts
+++ src/app/data/models/cart/base-price.ts
@@ -3,9 +3,9 @@ import { AmwayValue } from './amway-value';
 export class BasePrice {
     currencyIso: string;
     value: number;
-    priceType: string; // null,
-    formattedValue: string; // null,
-    minQuantity: number; // null,
-    maxQuantity: number; // null,
-    amwayValue: AmwayValue; // null
+    priceType: string; // enum (BUY, FROM)
+    formattedValue: string;
+    minQuantity: number; // integer(int64)
+    maxQuantity: number; // integer(int64)
+    amwayValue: AmwayValue; // ?
 }
--- src/app/data/models/cart/cart-data.ts
+++ src/app/data/models/cart/cart-data.ts
@@ -2,28 +2,31 @@ import { AmwayValue } from './amway-value';
 import { BasePrice } from './base-price';
 
 /**
- * Add to Cart Response 객체
+ * Add to Cart Response 객체 - CartModificationWsDTO
  *
  * null 로 되어 있어서 type을 알기 어려운 변수가 많음.
  */
 export class CartData {
     statusCode: string;
-    quantityAdded: number;
+    quantityAdded: number; // integer(int64)
     quantity: number;
     entry: Entry;
     deliveryModeChanged: boolean;
     statusMessage: string;
 }
 
+/**
+ * Order Entry - OrderEntryWsDTO
+ */
 export class Entry {
-    entryNumber: number;
-    quantity: number;
+    entryNumber: number; // integer(int32)
+    quantity: number; // integer(int64)
     basePrice: BasePrice;
     totalPrice: TotalPrice;
     product: Product;
     updateable: boolean;
-    deliveryMode: string;
-    deliveryPointOfService: string;
+    deliveryMode: null; // DeliveryModeWsDTO
+    deliveryPointOfService: null; // PointOfServiceWsDTO
     retailPrice: RetailPrice;
     pickupDateTime: string;
     aboBasePrice: number;
@@ -40,6 +43,9 @@ export class TotalPrice extends BasePrice { }
 
 export class Value extends AmwayValue { }
 
+/**
+ * ProductWsDTO
+ */
 export class Product {
     code: string;
     name: string;
@@ -47,42 +53,42 @@ export class Product {
     description: string;
     purchasable: boolean;
     stock: Stock;
-    futureStocks: number; // null,
+    futureStocks: null; // < FutureStockWsDTO > array
     availableForPickup: boolean;
-    averageRating: number; // null,
-    numberOfReviews: number; // null,
+    averageRating: number; // double
+    numberOfReviews: number; // integer(int32)
     summary: string;
     manufacturer: string;
     variantType: string;
     price: Price;
     baseProduct: string;
-    images: string;
-    categories: string;
-    reviews: string;
-    classifications: string;
-    potentialPromotions: string;
-    variantOptions: string;
-    baseOptions: string;
+    images: null; // < ImageWsDTO > array
+    categories: null; // < CategoryWsDTO > array
+    reviews: null; // < ReviewWsDTO > array
+    classifications: null; // < ClassificationWsDTO > array
+    potentialPromotions: null; // < PromotionWsDTO > array
+    variantOptions: null; // < VariantOptionWsDTO > array
+    baseOptions: null; // < BaseOptionWsDTO > array
     volumePricesFlag: boolean;
-    volumePrices: number;
-    productReferences: string;
+    volumePrices: null; // < PriceWsDTO > array
+    productReferences: null; // < ProductReferenceWsDTO > array
     variantMatrix: string;
-    priceRange: number;
-    multidimensional: string;
+    priceRange: null; // PriceRangeWsDTO
+    multidimensional: boolean;
     alias: string;
-    weight: number;
-    dimensions: number;
-    productAttributes: string;
-    onlineSince: string;
-    kitEntry: string;
-    kitPrice: number;
-    retailPrice: number;
-    deliveryModes: string;
+    weight: number; // double
+    dimensions: null; // AmwayDimensionWsDTO
+    productAttributes: string; // Map<String,String>
+    onlineSince: string; // Date
+    kitEntry: string; // Set<AmwayKitEntryProductWsDTO>
+    kitPrice: number; // double
+    retailPrice: RetailPrice; // PriceWsDTO
+    deliveryModes: null; // List<DeliveryModeWsDTO>
 }
 
 export class Stock {
     stockLevelStatus: string;
-    stockLevel: number; // null
+    stockLevel: number; // integer(int64)
 }
 
 export class Price extends BasePrice { }
