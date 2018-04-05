7a24afde88761dd0c1e417f9ee489241321bbf00
kr620345 <kr620345@amway.com>
2018-04-05 12:26:01 +0900

    서비스 정리
    서비스 정리

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -5,8 +5,7 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
 
-import { NetworkService } from './../core/peripheral/network/network.service';
-import { Config, Logger, LoginService } from './pos';
+import { Config, Logger, LoginService, NetworkService } from './pos';
 import { TerminalInfo, AccessToken } from '../data/model';
 import Utils from '../core/utils';
 
