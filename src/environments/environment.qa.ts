9fdae3e703c59562ffd104be998be7afb4baa9b6
kr620345 <kr620345@amway.com>
2018-04-05 18:20:03 +0900

    assets 포함 경로 중 config 경로 삭제 및 qa environment  추가
    assets 포함 경로 중 config 삭제 및 qa environment  추가

---
+++ src/environments/environment.qa.ts
@@ -0,0 +1,38 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  qzDebugMode: false,
+
+  hybrisEndpointDomain: 'www.abnkorea.co.kr',
+  hybrisEndpointPort: 80,
+
+  // 임시 - 추후 삭제
+  occEndpointDomain: '',
+
+  receitPolicyFile: '/assets/template/receipt/policy/receipt.json',
+
+  // pos configurations
+  baseSiteId: 'amwaykorea',
+  apiRootUrl: 'https://127.0.0.1:9002/amwaycommercewebservices/v2/',
+  apiUrl: {
+      terminal: 'https://127.0.0.1:9002/amwaycommercewebservices/v2/{baseSiteId}/auth/terminal',
+      auth: 'https://127.0.0.1:9002/amwaycommercewebservices/v2/{baseSiteId}/auth/authorize',
+      token: 'https://127.0.0.1:9002/authorizationserver/oauth/token',
+      batchStart: 'https://127.0.0.1:9002/amwaycommercewebservices/v2/{baseSiteId}/users/{user_id}/orderbatches',
+      createCart: 'https://127.0.0.1:9002/amwaycommercewebservices/v2/{baseSiteId}/accounts/{accountId}/users/{userId}/carts',
+      addToCart: 'https://127.0.0.1:9002/amwaycommercewebservices/v2/{baseSiteId}/users/{userId}/carts/{cartId}/entries',
+      updateVolAcc: 'https://127.0.0.1:9002/amwaycommercewebservices/v2/{baseSiteId}/users/{userId}/carts/{cartId}/volumeaccount'
+  },
+  terminalApiUrl: 'https://127.0.0.1:9002/amwaycommercewebservices/v2/amwaykorea/auth/terminal',
+  authApiUrl: 'https://127.0.0.1:9002/amwaycommercewebservices/v2/amwaykorea/auth/authorize',
+  tokenApiUrl: 'https://127.0.0.1:9002/authorizationserver/oauth/token',
+  logLevel: 'debug',
+  healthCheckUse: false,
+  healthCheckInterval: 9000,
+  hybrisCheckUrl: 'https://127.0.0.1:9002/amwaycommercewebservices/'
+
+};
