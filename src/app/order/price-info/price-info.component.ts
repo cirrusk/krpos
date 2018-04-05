46a5bf3e0b4145be9e4af24304a477d9cf8beb62
kr620345 <kr620345@amway.com>
2018-04-05 12:41:20 +0900

    소스정리
    소스정리

--- src/app/order/price-info/price-info.component.ts
+++ src/app/order/price-info/price-info.component.ts
@@ -1,5 +1,4 @@
-import { OnDestroy } from '@angular/core';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormArray, FormControl } from '@angular/forms';
 import { Subscription } from 'rxjs/Subscription';
 
