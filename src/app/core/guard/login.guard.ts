ce55777ad2085a6b079981420a7493b0ca30412b
kr620345 <kr620345@amway.com>
2018-04-09 14:23:01 +0900

    login guard 적용
    login guard 적용

--- src/app/core/guard/login.guard.ts
+++ src/app/core/guard/login.guard.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, CanActivateChild } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, CanActivateChild, Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
-import { StorageService } from '../../service/pos';
+import { StorageService, Logger } from '../../service/pos';
 
 @Injectable()
 export class LoginGuard implements CanActivate, CanActivateChild {
-  constructor(private storage: StorageService) { }
+  constructor(private storage: StorageService, private router: Router, private logger: Logger) { }
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
     if (this.storage.isLogin()) {
       return true;
+    }
+    const url = state.url;
+    if (url.indexOf('/order') !== -1) {
+      this.logger.debug('you are not login pos system, redirect to dashbord!', 'login.guard');
+      this.router.navigate(['/dashboard']);
     }
     return false;
   }
