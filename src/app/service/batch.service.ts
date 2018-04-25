a0d3eaefc3b25660955ad35cd5e36a73ac8494da
kr620345 <kr620345@amway.com>
2018-04-25 11:04:11 +0900

    배치 통계 정보 추가
    배치 통계 정보 추가

--- src/app/service/batch.service.ts
+++ src/app/service/batch.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/mergeMap';
 import { StorageService, Logger, ApiService } from './pos';
-import { BatchInfo, HttpData } from '../data/model';
+import { BatchInfo, BatchStats, HttpData } from '../data/model';
 import Utils from '../core/utils';
 
 @Injectable()
@@ -21,7 +21,7 @@ export class BatchService {
    * startingBalance: Starting Cash drawer balance. | (string) e.g 120
    * pickupStore: Store associated with terminal. | (string) e.g 01
    */
-  startBatch(): Observable <BatchInfo> {
+  startBatch(): Observable<BatchInfo> {
     this.logger.set('batch.service', 'Start shift start batch...').debug();
     const tokeninfo = this.storage.getTokenInfo();
     const terminalinfo = this.storage.getTerminalInfo();
@@ -37,7 +37,7 @@ export class BatchService {
    * 근무종료
    * 로그오프 시 배치 저장 후(POS 종료 확인 팝업 -> 배치 정보 저장  팝업 뜸) 대시보드 메인으로 이동
    */
-  endBatch(): Observable <BatchInfo> {
+  endBatch(): Observable<BatchInfo> {
     this.logger.set('batch.service', 'end batch...').debug();
     const batchinfo = this.storage.getBatchInfo();
     const batchid = batchinfo && batchinfo.batchNo;
@@ -45,7 +45,7 @@ export class BatchService {
     return this.api.put(data);
   }
 
-  endExistBatch(batchno: string): Observable <BatchInfo> {
+  endExistBatch(batchno: string): Observable<BatchInfo> {
     this.logger.set('batch.service', 'end exist batch...').debug();
     const data = new HttpData('batchStop', { batch_id: batchno }, null, {endingBalance: '0'} );
     return this.api.put(data);
@@ -59,24 +59,35 @@ export class BatchService {
    * 왜냐하면 브라우저 종료 후 다시 들어가면 Batch 세션 정보가 날라가므로
    * Batch 시작 안된 상태가 되며, 여기서 Batch 를 시작하면 already exist error 발생.
    */
-  getBatch(): Observable <BatchInfo> {
+  getBatch(): Observable<BatchInfo> {
     this.logger.set('batch.service', 'get current batch...').debug();
     const terminalinfo = this.storage.getTerminalInfo();
     const tid = terminalinfo && terminalinfo.id;
     const tnm = terminalinfo && terminalinfo.pointOfService.name;
-    const data = new HttpData('getBatch', null, null, { pickupStore: tnm, terminal: tid });
+    const data = new HttpData('batch', null, null, { pickupStore: tnm, terminal: tid });
     return this.api.get(data);
   }
 
   /**
    * 배치 조회와 삭제를 동시에 merge 한다.
    */
-  clearBatch(): Observable <BatchInfo> {
+  clearBatch(): Observable<BatchInfo> {
     this.logger.set('batch.service', 'get current batch and clear exist batch start...').debug();
     return this.getBatch()
     .flatMap((batchinfo: BatchInfo) => {
       return this.endExistBatch(batchinfo.batchNo);
     });
+  }
+
+  /**
+   * 배치 통계 정보 조회
+   */
+  statsBatch(): Observable<BatchStats> {
+    this.logger.set('batch.service', 'get batch statistics...').debug();
+    const batchinfo = this.storage.getBatchInfo();
+    const batchid = batchinfo && batchinfo.batchNo;
+    const data = new HttpData('batchStats', {batch_id: batchid}, null, null);
+    return this.api.get(data);
   }
 
 }
