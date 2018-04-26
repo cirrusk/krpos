20b5095e004ced766a11b4cd221aa096b56cd454
kr620345 <kr620345@amway.com>
2018-04-26 16:09:33 +0900

    수정
    수정

--- src/app/message/msg-en.ts
+++ src/app/message/msg-en.ts
@@ -1,5 +1,5 @@
 export const LANG_EN_NAME = 'en';
 export const LANG_EN_MSGS = {
-    'test': '테스트 메세지',
-    'msgtest': '메시지첫번째 : {0}, 두번째 : {1}'
+    'test': 'TEST MESSAGE',
+    'msgtest': 'MESSAGE FIRST : {0}, SECOND : {1}'
 };
