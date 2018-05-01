d91bb861d881b89cd1e350df0e05b303b7962894
kr620345 <kr620345@amway.com>
2018-05-01 10:45:37 +0900

    수정
    수정

--- src/app/message/msg-en.ts
+++ src/app/message/msg-en.ts
@@ -1,6 +1,5 @@
 export const LANG_EN_NAME = 'en';
 export const LANG_EN_MSGS = {
-    'test': 'TEST MESSAGE',
-    'msgtest': 'MESSAGE FIRST : {0}, SECOND : {1}',
+    'notSelectedUser' : '구매자를 선택해주세요.',
     'posNotSet': '미등록 POS 기기입니다. IT 관리자에게 문의하여 주시기 바랍니다.'
 };
