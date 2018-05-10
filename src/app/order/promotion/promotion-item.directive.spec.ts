6cec5912e5d40d2843d66da041f8c7f720ac55d4
kr620345 <kr620345@amway.com>
2018-05-10 16:42:30 +0900

    수정
    수정

--- src/app/order/promotion/promotion-item.directive.spec.ts
+++ src/app/order/promotion/promotion-item.directive.spec.ts
@@ -2,7 +2,7 @@ import { PromotionItemDirective } from './promotion-item.directive';
 
 describe('PromotionItemDirective', () => {
   it('should create an instance', () => {
-    const directive = new PromotionItemDirective();
+    const directive = new PromotionItemDirective(null);
     expect(directive).toBeTruthy();
   });
 });
