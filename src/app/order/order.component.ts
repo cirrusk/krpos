ec9b1189e5fe4b174c151b82a23e22ce6d534506
kr620345 <kr620345@amway.com>
2018-05-10 09:55:59 +0900

    최초 이벤트 발생
    최초 이벤트 발생

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LockType } from '../data';
+import { StorageService } from '../core';
 
 @Component({
   selector: 'pos-order',
@@ -7,9 +8,12 @@ import { LockType } from '../data';
 })
 export class OrderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private storage: StorageService) { }
 
   ngOnInit() {
+     // 장바구니 추가 시 클라이언트에 장바구니 데이터 전송
+     // 빈값을 한번 던져서 최초 이벤트를 발생시킴.
+    this.storage.setOrderEntry(null);
   }
 
 }
