cc0bd9e5b739d0fc5506cb1efc7b676f39268254
kr620345 <kr620345@amway.com>
2018-05-14 15:30:59 +0900

    수정
    수정

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -16,9 +16,11 @@ export class OrderComponent implements OnInit {
     // 장바구니 추가 시 클라이언트에 장바구니 데이터 전송
     // 빈값을 한번 던져서 최초 이벤트를 발생시킴.
     this.storage.setOrderEntry(null);
-    // resolver 에서 전달해준 값을 받아 cart list에 전달하여 공지사항에 뿌림.
+    // resolver 에서 전달해준 값을 받아 cart list에 전달하여 공지사항 출력
     const data = this.route.snapshot.data['notice'];
     this.noticeList = data['notice_ca'];
+    // resolver 에서 전달해준 값을 받아 cart menu에 전달하여 프로모션 출력
+    // api 가 다른 경우 resolver를 하나 더 만듬.
     this.promotionList = data['promotion'];
   }
 
