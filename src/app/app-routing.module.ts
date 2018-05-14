310f0bbdf86a842ddf6680248f3a4b6edb045e69
kr620345 <kr620345@amway.com>
2018-05-14 11:25:09 +0900

    공지사항 resolver 추가
    공지사항 resolver 추가

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,13 +9,14 @@ import { OrderComponent } from './order/order.component';
 import { OrderCompleteComponent } from './order/order-complete/order-complete.component';
 import { ClientComponent } from './order/client/client.component';
 import { TestComponent } from './common/test/test.component';
+import { NoticeResolver } from './order/notice/notice.resolver';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/dashboard', },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'order', component: OrderComponent, canActivate: [OrderGuard]},
+  { path: 'order', component: OrderComponent, canActivate: [OrderGuard], resolve: {notice: NoticeResolver}},
   { path: 'order-complete', component: OrderCompleteComponent, canActivate: [OrderGuard]},
-  { path: 'client', component: ClientComponent },
+  { path: 'client', component: ClientComponent, resolve: {notice: NoticeResolver} },
   { path: 'test', component: TestComponent },
   { path: '**', redirectTo: '/dashboard', pathMatch: 'full' }
 ];
@@ -27,6 +28,6 @@ const routes: Routes = [
   ],
   exports: [RouterModule],
   declarations: [],
-  providers: [LoginGuard, OrderGuard]
+  providers: [LoginGuard, OrderGuard, NoticeResolver]
 })
 export class AppRoutingModule { }
---
+++ src/app/order/notice/notice.resolver.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, Resolve } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { SearchService } from '../../service';
+
+@Injectable()
+export class NoticeResolver implements Resolve<any> {
+
+  constructor(private search: SearchService) {}
+  resolve(route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<any> {
+      const name = route.component && (<any>route.component).name;
+      let param = '';
+      if (name === 'ClientComponent') {
+        param = 'cl';
+      } else {
+        param = 'ca';
+      }
+      return this.search.getNoticeList(param);
+  }
+
+}
