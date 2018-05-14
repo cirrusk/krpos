0f96913ca69b359187159efd5f7d2a595817f94a
kr620345 <kr620345@amway.com>
2018-05-14 11:28:38 +0900

    캐셔용 공지사항 내용을 조회하여 cart list에 전달
    캐셔용 공지사항 내용을 조회하여 cart list에 전달

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { LockType } from '../data';
 import { StorageService } from '../core';
 
@@ -7,13 +8,16 @@ import { StorageService } from '../core';
   templateUrl: './order.component.html'
 })
 export class OrderComponent implements OnInit {
-
-  constructor(private storage: StorageService) { }
+  public noticeList: string[] = [];
+  constructor(private storage: StorageService, private route: ActivatedRoute) { }
 
   ngOnInit() {
      // 장바구니 추가 시 클라이언트에 장바구니 데이터 전송
      // 빈값을 한번 던져서 최초 이벤트를 발생시킴.
     this.storage.setOrderEntry(null);
+    const data = this.route.snapshot.data['notice'];
+    this.noticeList = data['notice_ca'];
+    console.log(this.noticeList);
   }
 
 }
