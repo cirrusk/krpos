070e217a3daf15a027481d1f26d191074432b8d1
kr620372 <kr620372@amway.com>
2018-06-11 15:08:28 +0900

    상품 수량 변경 수정

--- src/app/data/models/order/order-entries.ts
+++ src/app/data/models/order/order-entries.ts
@@ -3,10 +3,8 @@ import { Product } from '../common/product';
 export class OrderEntries {
     product: Product;
     quantity: string;
-    qty: string;
     constructor(product?: Product, quantity?: string) {
         this.product = product;
         this.quantity = quantity;
-        this.qty = quantity;
     }
 }
