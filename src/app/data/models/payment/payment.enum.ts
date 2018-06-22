a11e8b6e88f6f35a7b1e9a4b7052ac0eb13f806c
kr620345 <kr620345@amway.com>
2018-06-22 09:17:50 +0900

    StatusDisplay enum  추가
    StatusDisplay enum  추가

--- src/app/data/models/payment/payment.enum.ts
+++ src/app/data/models/payment/payment.enum.ts
@@ -1,3 +1,16 @@
+/** 주문 생성 시점에만 사용 */
+export enum StatusDisplay {
+    CREATED = 'created',               // 주문 생성
+    PROCESSING = 'processing',         // 주문 프로세스 진행 중
+    COMPLETED = 'completed',           // 주문 완료
+    ERROR = 'error',                   // 주문 오류
+    PAID = 'paid',                     // 결제 완료
+    CANCELLED = 'cancelled',           // 주문 취소 완료
+    CANCELLING = 'cancelling',         // 주문 취소 진행
+    PAYMENTFAILED = 'paymentfailed',   // 결제 처리 오류 – 주문 생성시 결제 캡처 오류, Cart는 삭제 되지 않은 상태
+    ORDERFAILED = 'orderfailed'        // 주문 생성 오류 – 주문 생성시 오류, Cart는 삭제 되지 않은 상태
+}
+
 export enum CashType {
     CASH = 'CASH',
     CHECK = 'CHECK'
