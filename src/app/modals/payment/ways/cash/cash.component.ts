9972167a40f5f5c06fcf15f3364d19947a69b628
kr620345 <kr620345@amway.com>
2018-06-28 22:42:15 +0900

    수정

--- src/app/modals/payment/ways/cash/cash.component.ts
+++ src/app/modals/payment/ways/cash/cash.component.ts
@@ -179,9 +179,11 @@ export class CashComponent extends ModalComponent implements OnInit, OnDestroy {
           this.printer.openCashDrawer(); // 캐셔 drawer 오픈
         } else if (this.finishStatus === StatusDisplay.PAYMENTFAILED) { // CART 삭제되지 않은 상태, 다른 지불 수단으로 처리
         } else { // CART 삭제된 상태
+          this.info.sendInfo('recart', this.orderInfo);
         }
       } else { // 결제정보 없는 경우, CART 삭제 --> 장바구니의 entry 정보로 CART 재생성
         // cart-list.component에 재생성 이벤트 보내서 처리
+        this.info.sendInfo('recart', this.orderInfo);
       }
       this.storage.removePay();
     }, error => {
