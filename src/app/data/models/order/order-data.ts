2bea2bc7641cb36111e96c2faf3fee5d314b170a
kr620345 <kr620345@amway.com>
2018-07-18 14:12:27 +0900

    주문내역 정렬 처리 수정
    주문내역 정렬 처리 수정

--- src/app/data/models/order/order-data.ts
+++ src/app/data/models/order/order-data.ts
@@ -1,36 +1,3 @@
-// export class OrderData {
-//     orderingABOId: string;
-//     phoneNumber: string;
-//     amwayBusinessNature: string;
-//     orderTypes: Array<string>; // NORMAL_ORDER
-//     salesChannels: Array<string>; // Web,WebMobile
-//     deliveryModes: Array<string>; // delivery,install
-//     statuses: Array<string>; // COMPLETE..
-//     confirm: boolean;
-//     currentPage: number;
-//     pageSize: number;
-//     orderCode: string;
-
-//     constructor(_orderTypes?: Array<string>,
-//         _salesChannels?: Array<string>,
-//         _deliveryModes?: Array<string>,
-//         _statuses?: Array<string>,
-//         _amwayBusinessNature?: string,
-//         _confirm?: boolean,
-//         _currentPage?: number,
-//         _pageSize?: number) {
-//         this.orderTypes = _orderTypes;
-//         this.salesChannels = _salesChannels;
-//         this.deliveryModes = _deliveryModes;
-//         this.statuses = _statuses;
-//         this.amwayBusinessNature = _amwayBusinessNature;
-//         this.confirm = _confirm;
-//         this.currentPage = _currentPage;
-//         this.pageSize = _pageSize;
-//     }
-// }
-
-
 export class OrderData {
     orderingABOId: string;
     volumeABOId: string;
