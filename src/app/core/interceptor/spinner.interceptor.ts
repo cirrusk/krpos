7ac5f782feccbd8b7fb0d97cc116e47d49f78f38
kr620345 <kr620345@amway.com>
2018-08-09 14:17:27 +0900

    수정

--- src/app/core/interceptor/spinner.interceptor.ts
+++ src/app/core/interceptor/spinner.interceptor.ts
@@ -9,6 +9,12 @@ import { SpinnerService } from '../spinner/spinner.service';
  * 기존에는 HttpClient subscribe 하기 전에 show 하고
  * error 또는 finally 에서 hide 해주는 구조로 구성하여
  * 모든 HttpClient 서비스에서 show / hide 코드 삽입해주어야 했음.
+ *
+ * 일반 컴포넌트에서 호출할 경우
+ * 스피너가 초기화되지 않은 상태에서
+ * show를 호출되는 경우 컴포넌트 인스턴스 생성 인터벌로 인해
+ * 스피너가 뜨지 않는 경우가 발생함.
+ * 이 경우 Interceptor를 사용할 경우 정상 동작함.
  */
 @Injectable()
 export class SpinnerInterceptor implements HttpInterceptor {
