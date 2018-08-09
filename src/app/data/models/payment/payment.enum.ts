a639b049b9e72eb736cf9e59035f8664e7401af1
kr620345 <kr620345@amway.com>
2018-08-09 13:06:50 +0900

    수정

--- src/app/data/models/payment/payment.enum.ts
+++ src/app/data/models/payment/payment.enum.ts
@@ -1,4 +1,7 @@
-/** 주문 생성 시점에만 사용 */
+/**
+ * 주문 상태 정보
+ * 주문 생성 시점에만 사용
+ * */
 export enum StatusDisplay {
     CREATED = 'created',               // 주문 생성
     PROCESSING = 'processing',         // 주문 프로세스 진행 중
@@ -11,27 +14,42 @@ export enum StatusDisplay {
     ORDERFAILED = 'orderfailed'        // 주문 생성 오류 – 주문 생성시 오류, Cart는 삭제 되지 않은 상태
 }
 
+/**
+ * 현금 타입
+ */
 export enum CashType {
     CASH = 'CASH',
     CHECK = 'CHECK'
 }
 
+/**
+ * 포인트 타입
+ */
 export enum PointType {
     BR022 = 'BR022', // 주문
     BR030 = 'BR030', // 전환포인트
     BR033 = 'BR033'  // 멤버포인트
 }
 
+/**
+ * 신용카드 지불 타입
+ */
 export enum CCPaymentType {
     GENERAL = 'GENERAL',
     SAFE = 'SAFE'
 }
 
+/**
+ * 신용카드 유형 타입
+ */
 export enum CCMemberType {
     PERSONAL = 'PERSONAL', // 개인카드
     LEGAL = 'LEGAL', // 법인카드
 }
 
+/**
+ * 지불 결제 처리 상태
+ */
 export enum PaymentStatus {
     CANCELLED = 'CANCELLED',
     NOTPAID = 'NOTPAID',
@@ -40,6 +58,9 @@ export enum PaymentStatus {
     PAID = 'PAID'
 }
 
+/**
+ * 지불 모드
+ */
 export enum PaymentModes {
     ARCREDIT = 'arCredit',
     CASH = 'cash',
@@ -51,16 +72,25 @@ export enum PaymentModes {
     COUPON = 'creditvoucher'
 }
 
+/**
+ * 계좌 유형
+ */
 export enum BankTypes {
     DIRECT_DEBIT = 'DIRECT_DEBIT',
     BONUS = 'BONUS'
 }
 
+/**
+ * 매입사 유형
+ */
 export enum VanTypes {
     NICE = 'NICE',
     LGU = 'LGU'
 }
 
+/**
+ * 제품 스캔 타입
+ */
 export enum ProductScanTypes {
     SERIALNUMBER = 'SERIAL_NUMBER',
     RFID = 'RFID'
