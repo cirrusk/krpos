b660f01d7463abe128073736e04b6f39f0f23ac2
kr620372 <kr620372@amway.com>
2018-09-03 10:49:02 +0900

    amway Value 수정

--- src/app/data/models/common/amway-value.ts
+++ src/app/data/models/common/amway-value.ts
@@ -2,4 +2,8 @@ export class AmwayValue {
     pointValue: number;
     businessVolume: number;
     volumeabo: number;
+    groupBusinessVolume: number;
+    groupPointValue: number;
+    personalBusinessVolume: number;
+    personalPointValue: number;
 }
