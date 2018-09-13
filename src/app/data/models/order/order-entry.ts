a9e06e5ccdb7dfecebe910513682b61644b04336
kr620345 <kr620345@amway.com>
2018-09-13 15:00:41 +0900

    주석 추가

--- src/app/data/models/order/order-entry.ts
+++ src/app/data/models/order/order-entry.ts
@@ -4,37 +4,39 @@ import { KitProductChildOrderEntry } from './cart-modification';
 export class OrderEntry {
     entryNumber: number;
     quantity: number;
-    basePrice: Price;
-    totalPrice: Price;
+    basePrice: Price; // 단가
+    totalPrice: Price; // 총 가격
     product: ProductInfo;
     updateable: boolean;
-    deliveryMode: DeliveryMode; // DeliveryModeWsDTO
-    deliveryPointOfService: PointOfService; // PointOfServiceWsDTO
+    deliveryMode: DeliveryMode; // 배송 방법
+    deliveryPointOfService: PointOfService; // 배송 지점
     url: string;
+    originalPrice: Price;
+    originalTax: Price;
     retailPrice: Price;
     pickupDateTime: Date;
     kitEntryCode: string;
     dispositionCode: any; // AmwayEnumData
     totalPriceInclTax: Price;
     isKitProductOrderEntry: boolean;
-    totalTax: Price;
+    totalTax: Price; // 세액
     bundleDescription: Array<KitProductChildOrderEntry>; // AmwayKitProductChildOrderEntryWsDTO
-    kitEntryNumber: number;
+    kitEntryNumber: number; // 키트 항목 번호
     aboBasePrice: number;
     retailBasePrice: number;
     proRatedPrice: number;
     margin: any; // AmwayOrderMarginWsDTO
-    value: AmwayValue;
+    value: AmwayValue;   // PV/BV
     skuversion: string;
     tes: string;
-    quantityAllocated: number;
-    quantityUnallocated: number;
-    quantityCancelled: number;
-    quantityPending: number;
-    quantityShipped: number;
-    quantityReturned: number;
+    quantityAllocated: number;   // 할당 수량
+    quantityUnallocated: number; // 할당되지 않은 수량
+    quantityCancelled: number;   // 취소 수량
+    quantityPending: number;     // 보류 수량
+    quantityShipped: number;     // 배송 수량
+    quantityReturned: number;    // 반품 수량
     ecpConfirmQty: any; // Ecp 컨펌 (숫자, true='완료')
-    serialNumbersCodes: Array<string>;
+    serialNumbersCodes: Array<string>; // 시리얼 넘버 (RFID 포함)
 
     constructor(_product?: ProductInfo) {
         this.product = _product;
