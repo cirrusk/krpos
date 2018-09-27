14628d8574d5e5af6cbec3fde1a76f2832d7509f
kr620372 <kr620372@amway.com>
2018-09-27 09:32:30 +0900

    소스 정리

--- src/app/order/cart-list/cart-list.component.ts
+++ src/app/order/cart-list/cart-list.component.ts
@@ -2276,7 +2276,7 @@ export class CartListComponent implements OnInit, OnDestroy {
         let selidx = 0;
         this.groups.forEach((group, idx) => { if (group.nativeElement.classList.contains('on')) { selidx = idx; } });
         if (selidx < glen - 1) {
-          this.selectedUserIndex = ++selidx;;
+          this.selectedUserIndex = ++selidx;
           const g = this.groups.find((group, idx) => idx === this.selectedUserIndex);
           this.choiceGroupUser(this.selectedUserIndex, g.nativeElement.getAttribute('data-uid'));
         }
